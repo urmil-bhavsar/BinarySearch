2c5d57f5cae541b6059d431de0324fd5e11c5a63
urmil-bhavsar <urmilbhavsar31@gmail.com>
2024-07-13 23:24:49 +0530

    search in a 2d array

---
+++ RowColMatrix.java
@@ -0,0 +1,31 @@
+// binary search in a 2d matrix
+
+import java.util.Arrays;
+
+public class RowColMatrix {
+    public static void main(String[] args) {
+        int[][] arr = {
+                { 10, 20, 30, 40 },
+                { 15, 25, 35, 45 },
+                { 28, 29, 37, 49 },
+        };
+        System.out.println(Arrays.toString(search(arr, 28)));
+    }
+
+    static int[] search(int[][] matrix, int target) {
+        int r = 0;
+        int c = matrix.length - 1;
+
+        while (r < matrix.length && c >= 0) {
+            if ((matrix)[r][c] == target) {
+                return new int[] { r, c };
+            }
+            if (matrix[r][c] < target) {
+                r++;
+            } else {
+                c--;
+            }
+        }
+        return new int[] { -1, -1 };
+    }
+}
